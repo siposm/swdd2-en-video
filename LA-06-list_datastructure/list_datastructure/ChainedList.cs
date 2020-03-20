db5d8049db85d1cb9c7bf1bdbce1d69eb0012d7f
siposm <sipos.miklos@nik.uni-obuda.hu>
2020-03-20 16:37:13 +0100

    list added

---
+++ LA-06-list_datastructure/list_datastructure/ChainedList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list_datastructure
+{
+    class ChainedList < T >
+    {
+        private ListItem head;
+
+        class ListItem
+        {
+            public T content;
+            public ListItem next;
+        }
+
+        // -----------------------------
+
+        public void Insert2Front(T newContent)
+        {
+            ListItem newItem = new ListItem();
+            newItem.content = newContent;
+            newItem.next = head; // !!!!
+            head = newItem;
+        }
+
+        public void Traversal()
+        {
+            ListItem p = head;
+            while (p != null)
+            {
+                Process(p);
+                p = p.next;
+            }
+        }
+
+        private void Process(ListItem p)
+        {
+            Console.WriteLine(p.content);
+        }
+
+        // --------------------
+
+        public int Count()
+        {
+            int count = 0;
+            ListItem p = head;
+            while (p != null)
+            {
+                count++;
+                p = p.next;
+            }
+            return count;
+        }
+
+        public T[] CopyToArray()
+        {
+            T[] arrayToReturn = new T[Count()];
+            int i = 0;
+
+            ListItem p = head;
+            while (p != null)
+            {
+                arrayToReturn[i++] = p.content;
+                p = p.next;
+            }
+
+            return arrayToReturn;
+        }
+    }
+}
---
+++ LA-06-list_datastructure/list_datastructure/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list_datastructure
+{
+    class Person
+    {
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChainedList<Person> personList =
+                new ChainedList<Person>();
+
+            personList.Insert2Front(new Person()
+            {
+                Name = "Test Person 1"
+            });
+            personList.Insert2Front(new Person()
+            {
+                Name = "Test Person 2"
+            });
+            personList.Insert2Front(new Person()
+            {
+                Name = "Test Person 3"
+            });
+
+            personList.Traversal();
+
+            foreach (var item in personList.CopyToArray())
+            {
+                Console.WriteLine(">>> " + item);
+            }
+
+            // ---------------------
+
+            ChainedList<int> intList =
+                new ChainedList<int>();
+
+            intList.Insert2Front(20);
+            intList.Insert2Front(200);
+            intList.Insert2Front(34);
+
+            intList.Traversal();
+
+            // ---------------------
+
+            ChainedList<string> stringList
+                = new ChainedList<string>();
+
+            stringList.Insert2Front("first item");
+            stringList.Insert2Front("second item");
+            stringList.Insert2Front("third item");
+
+            stringList.Traversal();
+
+            foreach (var item in stringList.CopyToArray())
+            {
+                Console.WriteLine("---> " + item);
+            }
+        }
+    }
+}
