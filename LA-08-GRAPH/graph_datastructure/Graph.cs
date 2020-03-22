e9407f4471b27cb14312a64982cc10eb951b00fe
siposm <sipos.miklos@nik.uni-obuda.hu>
2020-03-22 15:04:58 +0100

    added graph

---
+++ LA-08-GRAPH/graph_datastructure/Graph.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graph_datastructure
+{
+    class Graph<T>
+    {
+        List<List<T>> adjacencyList = new List<List<T>>();
+        List<T> content = new List<T>();
+
+        public void AddNode(T node)
+        {
+            content.Add(node);
+            adjacencyList.Add(new List<T>());
+        }
+
+        public void AddEdge(T from, T to)
+        {
+            int indexFrom = content.IndexOf(from);
+            int indexTo = content.IndexOf(to);
+
+            adjacencyList[indexFrom].Add(content[indexTo]);
+            adjacencyList[indexTo].Add(content[indexFrom]);
+        }
+
+        public bool HasEdge(T from, T to)
+        {
+            int indexFrom = content.IndexOf(from);
+            int indexTo = content.IndexOf(to);
+
+            return adjacencyList[indexFrom].Contains(content[indexTo]);
+        }
+
+        public List<T> Neighbors(T whichNode)
+        {
+            int index = content.IndexOf(whichNode);
+            return adjacencyList[index];
+        }
+
+        public void DFS(T startNode)
+        {
+            List<T> F = new List<T>();
+            DFS(startNode, F);
+        }
+
+        private void DFS(T k, List<T> F)
+        {
+            F.Add(k);
+            Console.WriteLine(k.ToString()); // !!! 
+
+            foreach (T x in Neighbors(k))
+                if (!F.Contains(x))
+                    DFS( x, F );
+        }
+
+        public void BFS(T startNode)
+        {
+            Queue<T> S = new Queue<T>();
+            List<T> F = new List<T>();
+
+            S.Enqueue(startNode);
+            F.Add(startNode);
+
+            T k;
+
+            while ( S.Count != 0 )
+            {
+                k = S.Dequeue();
+                Console.WriteLine(k.ToString());
+                foreach (T x in Neighbors(k))
+                {
+                    if ( ! F.Contains(x) )
+                    {
+                        S.Enqueue(x);
+                        F.Add(x);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LA-08-GRAPH/graph_datastructure/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graph_datastructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph<string> g = new Graph<string>();
+
+            g.AddNode("Joseph");
+            g.AddNode("Stew");
+            g.AddNode("Marge");
+            g.AddNode("Gerald");
+            g.AddNode("Zack");
+            g.AddNode("Peter");
+            g.AddNode("Janet");
+
+            g.AddEdge("Joseph", "Stew");
+            g.AddEdge("Joseph", "Marge");
+            g.AddEdge("Marge", "Stew");
+
+            g.AddEdge("Joseph", "Zack");
+            g.AddEdge("Gerald", "Zack");
+            g.AddEdge("Joseph", "Gerald");
+
+            g.AddEdge("Peter", "Zack");
+            g.AddEdge("Peter", "Janet");
+
+            Console.WriteLine("\n>> DFS\n");
+            g.DFS("Janet");
+
+            Console.WriteLine("\n>> BFS\n");
+            g.BFS("Janet");
+        }
+    }
+}
