fa739a12805a5549df3bbb24ccebaded68e9b8d9
siposm <sipos.miklos@nik.uni-obuda.hu>
2020-04-17 18:34:08 +0200

    added queue class

--- LA-EXTRA-QUEUE_AND_STACK/queue_and_stack_datastructure/MyQueue.cs
+++ LA-EXTRA-QUEUE_AND_STACK/queue_and_stack_datastructure/MyQueue.cs
@@ -6,7 +6,64 @@ using System.Threading.Tasks;
 
 namespace queue_and_stack_datastructure
 {
-    class MyQueue
+    class MyQueue<T>
     {
+        T[] items;
+        int count;
+
+        public delegate void TraversalHandler(T content);
+
+        public MyQueue(int size)
+        {
+            items = new T[size];
+            count = -1;
+        }
+
+        public T Peek()
+        {
+            if (count >= 0)
+                return items[0];
+            else
+                throw new Exception("Queue is empty.");
+        }
+
+        public void Enqueue(T newItem)
+        {
+            if ( count < items.Length )
+                items[++count] = newItem;
+            else
+                throw new Exception("Queue is full.");
+        }
+
+        public T Dequeue()
+        {
+            if (count >= 0)
+            {
+                T toReturn = items[0];
+
+                Shift();
+
+                items[count] = default;
+                count--;
+
+                return toReturn;
+            }
+            else
+                throw new Exception("Queue is empty.");
+        }
+
+        public void Traverse(TraversalHandler method)
+        {
+            for (int i = 0; i <= count; i++)
+                method(items[i]);
+        }
+
+        // ------------
+
+        private void Shift()
+        {
+            for (int i = 1; i <= count; i++)
+                items[i - 1] = items[i];
+        }
     }
 }
--- LA-EXTRA-QUEUE_AND_STACK/queue_and_stack_datastructure/Program.cs
+++ LA-EXTRA-QUEUE_AND_STACK/queue_and_stack_datastructure/Program.cs
@@ -8,8 +8,53 @@ namespace queue_and_stack_datastructure
 {
     class Program
     {
+        static void TraversalMethod(string input)
+        {
+            Console.WriteLine("\t>> " + input);
+        }
+
+        static void QueueTesting()
+        {
+            MyQueue<string> mq = new MyQueue<string>(7);
+
+            try
+            {
+                mq.Peek();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[ERR] : " + e.Message);
+            }
+
+            mq.Enqueue("apple 1");
+            mq.Enqueue("apple 2");
+            mq.Enqueue("apple 3");
+            mq.Enqueue("apple 4");
+
+            Console.WriteLine("QUEUE ITEMS:");
+            mq.Traverse(TraversalMethod);
+            Console.WriteLine();
+
+            Console.WriteLine("PEEK 1: " + mq.Peek()); // should return "apple 1"
+
+            Console.WriteLine("TAKE 1: " + mq.Dequeue()); // should return "apple 1"
+            Console.WriteLine("TAKE 2: " + mq.Dequeue()); // should return "apple 2"
+
+            Console.WriteLine("PEEK 2: " + mq.Peek()); // should return "apple 3"
+        }
+
         static void Main(string[] args)
         {
+            // :::::::::::::::::::::::::::::::::::::::::
+            //  QUEUE DATA STRUCTURE - EXAMPLE
+            // :::::::::::::::::::::::::::::::::::::::::
+
+            QueueTesting();
+            
+
+            // :::::::::::::::::::::::::::::::::::::::::
+            //  STACK DATA STRUCTURE - EXAMPLE
+            // :::::::::::::::::::::::::::::::::::::::::
         }
     }
 }
